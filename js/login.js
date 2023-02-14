bdc559ef202dbfea6f06a238b20b1f0dd338f253
NAZMUS SAKIB <76749935+nazmus-sakibb@users.noreply.github.com>
2023-02-14 15:40:13 +0600

    make input field interactive

--- js/login.js
+++ js/login.js
@@ -1,4 +1,12 @@
-// Add a click event handler in the submit button
+//step-1 Add a click event handler in the submit button
 document.getElementById('btn-submit').addEventListener('click', function(){
-    console.log('Submit button clicked');
+    //step-2 get the email address inside the input field
+    const emailField = document.getElementById('user-email'); 
+    const email = emailField.value;
+
+    //step-3 get the password
+    const passwordField = document.getElementById('user-password');
+    const password = passwordField.value;
+    console.log(email, password); 
 })
+
