f7fb534a9db8d81f688a1aaab71fb284b1e6466c
NAZMUS SAKIB <76749935+nazmus-sakibb@users.noreply.github.com>
2023-02-18 02:04:59 +0600

    deduct withdrawal from the total balance

--- js/withdraw.js
+++ js/withdraw.js
@@ -1,6 +1,11 @@
 /***
  * 1. add event handler with the withdraw button
  * 2. get the withdraw amount
+ * 3. get previous withdraw total
+ * 4. calculate total withdraw amount
+ * 5. get the previous balance total
+ * 6. calculate new balance total and set 
+ * 7. clear input field
  */
 
 // step 1 
@@ -9,5 +14,23 @@ document.getElementById('btn-withdraw').addEventListener('click', function(){
     const withdrawField = document.getElementById('withdraw-field');
     const newWithdrawAmountString = withdrawField.value;
     const newWithdrawAmount = parseFloat(newWithdrawAmountString);
-    console.log(newWithdrawAmount);
+
+    //step 3
+    const withdrawTotalElement = document.getElementById('withdraw-total');
+    const previousWithdrawTotal = parseFloat(withdrawTotalElement.innerText);
+    
+    //step 4
+    const currentWithdrawTotal = previousWithdrawTotal + newWithdrawAmount;
+    withdrawTotalElement.innerText = currentWithdrawTotal;
+
+    //step 5
+    const balanceTotalElement = document.getElementById('balance-total');
+    const previousBalanceTotal = parseFloat(balanceTotalElement.innerText);
+
+    //step 6
+    const newBalanceTotal = previousBalanceTotal - newWithdrawAmount;
+    balanceTotalElement.innerText = newBalanceTotal;
+
+    //step 7
+    withdrawField.value = '';
 })
