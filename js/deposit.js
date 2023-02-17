6e8bb1c17ca7bb53d43610cc29c6fda4c09d1cd6
NAZMUS SAKIB <76749935+nazmus-sakibb@users.noreply.github.com>
2023-02-17 23:25:47 +0600

    update deposit from the input field

--- js/deposit.js
+++ js/deposit.js
@@ -3,5 +3,9 @@ document.getElementById('btn-deposit').addEventListener('click', function(){
     // step 2 - get the deposit amount form the deposit field
     const depositField = document.getElementById('deposit-field');
     const depositAmount = depositField.value;
-    console.log(depositAmount);
+    
+    // step 3 - get the deposit total
+    const depositTotalElement = document.getElementById('deposit-total');
+    const depositTotal = depositTotalElement.innerText;
+    depositTotalElement.innerText = depositAmount;
 })
